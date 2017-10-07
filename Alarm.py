2167ece85ce9aeb718bface323f1e6fa64e05a4f
mharvy <32440398+mharvy@users.noreply.github.com>
2017-10-07 17:45:04 -0500

    Formatted correctly, changed functions
    Making the method that waits until alarm time not use any loops, but instead use the time.sleep(time) function. This should make it take up less resources. Note that it does not work quite yet

--- Alarm.py
+++ Alarm.py
@@ -1,58 +1,39 @@
-# Import libraries for alarm
 import os
 import random
 import webbrowser
 from time import strftime, localtime, sleep
-
-# Import libraries for parsing youtube
 import urllib.request
 import lxml.html
 
 
-# If YT file doesn't exist, create one
-if not(os.path.exists('YT.txt')):
-    flags = os.O_RDWR|os.O_CREAT
+# Writes youtube video urls to YT.txt
+def write_vids():
+    
+    connection = urllib.request.urlopen('https://www.youtube.com/music')
+    dom = lxml.html.fromstring(connection.read())
+    if os.path.exists('YT.txt'):
+        
+        os.remove('YT.txt')
     f = open('YT.txt', 'x')
+    for link in dom.xpath('//a/@href'): # select the url in href for all a tags(links)
+    
+        if '/watch?v=' in link:
+            
+            f.write('https://youtube.com/' + link + "\n")
     f.close()
 
 
-# Fill YT file with new youtube links
-connection = urllib.request.urlopen('https://www.youtube.com/music')
-dom = lxml.html.fromstring(connection.read())
-
-
-# Writes youtube video urls to YT.txt
-if os.path.exists('YT.txt'):
-    os.remove('YT.txt')
-f = open('YT.txt', 'x')
-
-for link in dom.xpath('//a/@href'): # select the url in href for all a tags(links)
-    if '/watch?v=' in link:
-        f.write('https://youtube.com/' + link + "\n")
-
-f.close()
-
-
-# Prompts user for alarm time
-print('When do you want to wake up?\nExample: 06:30')
-alarm_time = input("> ")
-
-
-# Variable for system time
-system_time = strftime("%H:%M", localtime())
-
-
 # Waits for time to approach alarm time
-while system_time != alarm_time:
-    system_time = strftime("%H:%M", localtime())
-    print("The time is now " + system_time)
-    sleep(60 - int(strftime("%S")))
-    system_time = strftime("%H:%M", localtime())
+def alarm_function(alarm_time):
 
+    system_time = strftime("%H:%M", localtime())
+    
+    time_to_sleep = (int(alarm_time[:2]) % int(system_time[:2]) * 3600) + 
+    sleep( int(alarm_time[:2]) * 3600 + int(strftime('%M') )
 
-# Activates alarm
-if strftime("%H:%M", localtime()) == alarm_time:
-    print("The time is now " + system_time + "\nTime to wake up!")
+    # Activates alarm
+    if strftime("%H:%M", localtime()) == alarm_time:
+        print("The time is now " + system_time + "\nTime to wake up!")
 
     # Plays video from YT.txt file
     f_lines = open('YT.txt', 'r').read().splitlines()
@@ -61,8 +42,14 @@ if strftime("%H:%M", localtime()) == alarm_time:
     exit()
 
 
+def main():
+    
+    alarm_time = input("What time do you want to wake up?\nExample: 06:30\n>   ")
+    alarm_function(alarm_time)  
 
 
-
+if __name__ == '__main__':
+          
+    main()
 
 
